131e7a7308acc151812ac5b10081af826ae3deb8
Dominik <dominik_p@wp.pl>
2019-10-18 11:20:47 +0200

    added connection to db

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { ApolloServer } = require('apollo-server-express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-kszkn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+    .then(
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    )
+    .catch(err => console.log(err))
